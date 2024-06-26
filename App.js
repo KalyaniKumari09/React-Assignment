77342fbd445e0c0b62a9a4b30da8acdca9ed7c2d
KalyaniKumari09 <113120303+KalyaniKumari09@users.noreply.github.com>
2024-06-26 18:03:52 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Login from './components/Login';
+import Signup from './components/Signup';
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+      <Signup />
+    </div>
+  );
+}
+
+export default App;
+
+
